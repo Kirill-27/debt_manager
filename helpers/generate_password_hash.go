3877f03c7064b4f9b597489ebac7fbd666433465
Kirill Chernov <kirillchernov01@gmail.com>
2023-06-09 12:13:19 +0200

    sha256

--- helpers/generate_password_hash.go
+++ helpers/generate_password_hash.go
@@ -1,14 +1,14 @@
 package helpers
 
 import (
-	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 )
 
 const salt = "h0q12hqw124f17ajf3ajs"
 
 func GeneratePasswordHash(password string) string {
-	hash := sha1.New()
+	hash := sha256.New()
 	hash.Write([]byte(password))
 
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
